499a0a480d24713dff6d5e9f86e2b55537d3d62a
Dhananjay-JSR <ajaysenday@gmail.com>
2022-12-18 03:54:56 +0530

    Nurture Component Added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 // import StartUpPage from "./Components/StartupPage";
 import LoginPage from "./Components/Screens/LoginPage"
+import NurturePage from "./Components/Screens/NurturerPage"
 
 export default function App (){
-  return <LoginPage/>
+  // return <LoginPage/>
+  return <NurturePage/>
 }
