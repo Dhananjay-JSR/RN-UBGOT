ad45ef5692b588afc7e0d6eb3554d2101cbfd6b0
Dhananjay-JSR <ajaysenday@gmail.com>
2022-12-18 07:45:50 +0530

    Main Page Done

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 // import StartUpPage from "./Components/StartupPage";
+import HomePage from "./Components/Screens/HomePage"
 import InspirePage from "./Components/Screens/InspirePage"
 import LoginPage from "./Components/Screens/LoginPage"
 import MeditatePage from "./Components/Screens/MeditatePage"
@@ -8,5 +9,8 @@ export default function App (){
   // return <LoginPage/>
   // return <NurturePage/>
   // return <InspirePage/>
-  return <MeditatePage/>
+  // return <MeditatePage/>
+  return <HomePage/>
+
+
 }
